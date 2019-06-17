b52d5775c3ff10d11801e18af4c0ab039dfe5297
Dominik <uJexxi@gmail.com>
2019-06-17 15:55:41 +0200

    Added dark mode, fixed image dimensions

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ var version_button = document.getElementById("version");
 var target = document.getElementById("img_target");
 var reset_btn = document.getElementById("reset_btn");
 var filter_input = document.getElementById("filter_input");
+var dark_mode_btn = document.getElementById("dark_mode");
 
 //Function that loads images.json from the storage
 //Don't even ask, it's JS magicfuckery..
@@ -102,6 +103,8 @@ document.addEventListener("keypress", function (e) {
 //Show an alert with version updates
 version_button.addEventListener("click", function () {
     alert(
+        "Version 1.1.3\n" +
+        "Added Dark Mode.\n\n" +
         "Version 1.1.2\n" +
         "Revamped filtering once again.\n" +
         "  Filtering now supports dynamic results, user does not need to input a whole name of image\n\n" +
@@ -112,4 +115,25 @@ version_button.addEventListener("click", function () {
         "Version 1.0.1\n" +
         "Edited markup.\nConverted the app into CSS grid.\n"
     );
+})
+
+//Function changes document's body background
+//And changes button icons (white & black)
+dark_mode_btn.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+    var bulb_dark = document.getElementById("bulb_d");
+    var bulb_light = document.getElementById("bulb_l");
+    var eraser_dark = document.getElementById("eraser_d");
+    var eraser_light = document.getElementById("eraser_l");
+    if (document.body.classList.contains("dark")) {
+        bulb_dark.style.display = "none";
+        bulb_light.style.display = "block";
+        eraser_dark.style.display = "none";
+        eraser_light.style.display = "inline-block";
+    } else {
+        bulb_dark.style.display = "block";
+        bulb_light.style.display = "none";
+        eraser_dark.style.display = "inline-block";
+        eraser_light.style.display = "none";
+    }
 })
